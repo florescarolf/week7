6bb1ad4afdaee31dbf3430d802e9c6156e3b0a55
florescarolf <carolinacostaflores@gmail.com>
2023-11-23 16:53:34 +0000

    Added Weather Functions

--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@
 function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
-     let cityElement = document.querySelector("#city"); 
+    let cityElement = document.querySelector("#city"); 
+    let descriptionElement = document.querySelector("#description"); 
+    let humidityElement = document.querySelector("#humidity");
+    let speedElement = document.querySelector("#speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000)
+        
+        
+    console.log(response.data);
+
+
     cityElement.innerHTML = response.data.city;
-   
-   
+    descriptionElement.innerHTML = response.data.condition.description; 
     temperatureElement.innerHTML = Math.round(temperature);
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    speedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    timeElement.innerHTML = formatDate(date);
 }
+
 
 
 function searchCity(city) {
@@ -17,14 +30,27 @@ function searchCity(city) {
 }
 
 
-
-
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
- 
     searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Lisbon");
+
+function formatDate(date) {
+    
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`; 
+    } 
+        
+    return `${day}, ${hour}:${minutes}`;
+}
